cc0f2cbaaa3bc9b61cbe25215e0afffe8036a343
FerminKrause <fkrausea@hotmail.com>
2020-12-30 23:39:52 -0300

    Generación de release 1.0.0 para la aplicación MiAPR.

--- app/src/main/java/com/example/miapr/RegistrarDatosMedidor.java
+++ app/src/main/java/com/example/miapr/RegistrarDatosMedidor.java
@@ -1,8 +1,5 @@
 package com.example.miapr;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -12,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
