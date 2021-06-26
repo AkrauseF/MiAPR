7a2b82b8c774cbc50b0b36451753f27c24f59f4d
FerminKrause <fkrausea@hotmail.com>
2021-06-26 19:30:16 -0400

    Modificación en menu app web

--- appWeb/menu.php
+++ appWeb/menu.php
@@ -8,8 +8,8 @@
   <li><a href="pagos.php">Pagos</a></li>
   <li><a href="generarInforme.php">Informe Subsidiarios</a></li>
   <li><a href="busquedaPagos.php">Buscador de Pagos</a></li>
-  <li><a href="secretarios.php">Secretarios</a></li>
-  <li><a href="operadores.php">Operadores</a></li>
+  <li><a >Secretarios</a></li>
+  <li><a >Operadores</a></li>
 
   
   
